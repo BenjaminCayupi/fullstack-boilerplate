e3003826aff24ceda53391ccd626d27892cb55d7
benjaminCayupi <benjamin.cayupi@gmail.com>
2025-03-14 16:09:57 -0300

    Adds base theme configuration

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/ui/button';
+
 const RootPage = () => {
   return (
     <main>
-      <div className="">Hello world</div>
+      <div className="container mx-auto">Hello world</div>
     </main>
   );
 };
